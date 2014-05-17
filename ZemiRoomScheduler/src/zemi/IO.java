50603964157fbbdd8d384df12c3b94dc53389bb0
akihiro <yamamori.a.aa@m.titech.ac.jp>
2014-05-17 20:28:11 +0900

    Imported OrangeSignalCSV

--- ZemiRoomScheduler/src/zemi/IO.java
+++ ZemiRoomScheduler/src/zemi/IO.java
@@ -1,10 +1,22 @@
 package zemi;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import jp.sf.orangesignal.csv.Csv;
+import jp.sf.orangesignal.csv.CsvConfig;
+import jp.sf.orangesignal.csv.handlers.StringArrayListHandler;
+
 public class IO {
 	static ArrayList<Schedule> input() {
+		try {
+			List<String[]> list = Csv.load(new File("test.csv"), new CsvConfig(), new StringArrayListHandler());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
